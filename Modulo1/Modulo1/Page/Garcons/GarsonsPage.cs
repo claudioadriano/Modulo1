0b971bd2bd6c6b51af7b39ac827daf06ffe47029
Claudio <Claudio@DESKTOP-I584DPG>
2017-02-09 23:06:51 -0200

    Add project files.

---
+++ Modulo1/Modulo1/Page/Garcons/GarsonsPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Modulo1.Pages.Garcons
+{
+    public class GarconsPage : TabbedPage
+    {
+        public GarconsPage()
+        {
+            Children.Add(new GarconsListPage()
+            {
+                Title = "Listagem",
+                Icon = "icone_list.png"
+            });
+            Children.Add(new GarconsNewPage()
+            {
+                Title = "Inserir Novo",
+                Icon = "icone_new.png"
+            });
+        }
+    }
+}
---
+++ Modulo1/Modulo1/Page/MenuPage.cs
@@ -0,0 +1,29 @@
+﻿using Modulo1.Pages.Garcons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Modulo1.Pages
+{
+    public class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            Title = "Menu de opções";
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+new Button() {
+Text = "Garçons",
+Image = "icone_garcons.png",
+Command = new Command(() => Navigation.PushAsync(new GarconsPage()))
+}
+}
+            };
+        }
+    }
+}
